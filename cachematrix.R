95f90f87ff31987f2918ec64003f965608ecb94f
Neeraj Bhatnagar <neerajbwrk@yahoo.com>
2015-02-16 08:28:31 -0800

    Committing the changes to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Written by Neeraj Bhatnagar on 2/15/2015
 
-## Write a short comment describing this function
+## This matrix inversion programs demonstartes the use of R's <<- function.
+## If no valid value exists in the cache, it computes the inverse of a 
+## matrix using R's solve function and caches the result.  If a valid 
+## value exists, it returns the cached value.    If the value of the
+## matrix itself is changed then the cached value is invalidated. 
+
+
+## The function computes and stores (caches) the inverse of a matrix.
+## Cached value is either null or is consistent with the current value of 
+## the matrix.
+## THE SUPPLIED MATRIX MUST BE INVERTIBLE.  IF NOT, TRULY BAD 
+## UNPREDICTABLE THINGS MAY HAPPEN.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+         I <- NULL
+         set <- function(y) {
+                 x <<- y
+                 I <<- NULL
+         }
+         get <- function() x
+         setInverse <- function(inverse) I <<- inverse
+         getInverse <- function() I
+         list(set = set, get = get,
+              setInverse = setInverse,
+              getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## If the matrix is modified since the last call, recompute and recache the 
+## inverse.  Otherwise return the cached value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        I <- x$getInverse()
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+        data <- x$get()
+        I <- solve(data, ...)
+        x$setInverse(I)
+        I
 }
